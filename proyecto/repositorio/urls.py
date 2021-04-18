840fc88eb1b95088f0e8cec7077f746936fec50b
Martin Hurtado <martindrumer21@gmail.com>
2021-04-17 18:53:02 -0700

    Crear los controladores de inicio e importar alumnos y configurar urls. Crear los controladores "ControladorInicio" y "ControladorImportarAlumnos", Configure las urls para proyecto y repositorio, y añadi mas librerias al archivo librerias_necesarias.txt.

---
+++ proyecto/repositorio/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.ControladorInicio, name='inicio'),
+    path('/importar', views.ControladorImportarAlumnos, name='importar_alumnos'),
+]
--- proyecto/repositorio/views.py
+++ proyecto/repositorio/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def ControladorInicio(request):
+    return HttpResponse("Inicio")
+
+def ControladorImportarAlumnos(request):
+    return HttpResponse("Importar Alumnos")
