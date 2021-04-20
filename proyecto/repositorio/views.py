61c500c6210375fa78cf49b4fdbe13068c722ec9
Martin Hurtado <martindrumer21@gmail.com>
2021-04-19 21:38:37 -0700

    Crear archivos de ejemplo y crear ruta de templates. Cree archivos de ejemplo para ayudar a mi equipo a familiarizarse con el Framework Django, tambien modifique el controlador del inicio para demostrar la herencia de templates.

--- proyecto/repositorio/views.py
+++ proyecto/repositorio/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def ControladorInicio(request):
-    return HttpResponse("Inicio")
+    # render() es la manera mas facil de enviar html por una respuesta.
+    # request es un objeto peticion con la informacion del navegador, etc.
+    # template_name es un string con la ruta del template.
+    return render(request, template_name = "ejemplo_de_vista.html")
 
 def ControladorImportarAlumnos(request):
     return HttpResponse("Importar Alumnos")
