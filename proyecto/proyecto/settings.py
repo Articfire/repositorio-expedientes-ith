5721c363c6802d1f84c271a3675eb07ea1980552
Martin Hurtado <martindrumer21@gmail.com>
2021-04-21 11:38:36 -0700

    ImportarAlumnos y el panel de adminstracion ya funcionan.

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
+
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-p1y#y3sa#$i5i+kesqjx%dv=9)s2(zpf!^yzsa3+ec@8l!kfd(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -124,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- proyecto/repositorio/views.py
+++ proyecto/repositorio/views.py
@@ -1,20 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
 import pandas as pd
+
+from .models import Alumno
 
 # Create your views here.
 def ControladorInicio(request):
+    '''
     # render() es la manera mas facil de enviar html por una respuesta.
     # request es un objeto peticion con la informacion del navegador, etc.
     # template_name es un string con la ruta del template.
-    return render(request, template_name = "ejemplo_de_vista.html")
+    '''
+    return HttpResponse("Inicio")
 
 def ControladorImportarAlumnos(request):
+    '''docstring para ControladorImportarAlumnos.'''
+    data = {'error': None}
     if request.method == 'POST':
-        try:
-            # info_excel = pd.read_excel(r'ruta_al_archivo.xlsx')
-            # info_excel.to_dict('index')
-            pass
-        except Exception as e:
-            print(e)
-    return HttpResponse("Importar Alumnos")
+        if request.FILES.get('archivo'):
+            mi_archivo = request.FILES.get('archivo')
+
+            if '.xlsx' in mi_archivo.name:
+                fs = FileSystemStorage()
+                nombre_archivo = fs.save(mi_archivo.name, mi_archivo)
+
+                info_excel = pd.read_excel(r'media/{}'.format(mi_archivo.name))
+                diccionario_alumnos = info_excel.to_dict('index')
+
+                for fila in diccionario_alumnos.values():
+                    alumno_nuevo = Alumno(**fila)
+                    alumno_nuevo.save()
+            else:
+                data['error'] = 'El archivo subido no es de excel, suba uno de excel (con la terminacion .xlsx).'
+        else:
+            data['error'] = 'No subio ningun archivo, porfavor elija uno y subalo.'
+    return render(request, 'importar.html', data)
