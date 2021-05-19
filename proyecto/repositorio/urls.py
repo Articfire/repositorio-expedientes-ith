1d932adbdb55d0e58d5fd131584a2b7cedc55fe7
Martin Hurtado <martindrumer21@gmail.com>
2021-05-18 20:48:41 -0700

    Crear url y view para Expediente y Eliminar template redundante. Tambien añadir el directorio "media" a .gitignore para evitar problemas con el peso del proyecto, y remover una opcion del header que apuntaba a un template innecesario.

--- proyecto/repositorio/urls.py
+++ proyecto/repositorio/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.ControladorInicio, name='inicio'),
     path('importar', views.ControladorImportarAlumnos, name='importar_alumnos'),
     path('alta', views.ControladorAltaAlumnos, name='alta_alumnos'),
-    path('subir_archivos', views.ControladorSubirArchivos, name='subir_archivos'),
+    path('expediente/<int:id>', views.ControladorExpediente, name='expediente'),
 ]
